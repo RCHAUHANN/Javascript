5094e654023b9aa01a5d2387f690f91d5672b6fd
RCHAUHANN <97589219+RCHAUHANN@users.noreply.github.com>
2022-12-09 08:21:25 +0200

    .

---
+++ module3/module3-starters-main/t3/3.js
@@ -0,0 +1,8 @@
+'use strict';
+const names = ['John', 'Paul', 'Jones'];
+
+for (let i = 0; i < names.length; i++){
+    const li = document.createElement('li');
+    li.innerHTML=names[i];
+    ul.appendChild(li);
+   }
